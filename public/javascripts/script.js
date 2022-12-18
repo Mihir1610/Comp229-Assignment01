b7e5839780d6049d1bcd2cadb4499a7425764256
Mihir Darji <mihirdarji71@gmail.com>
2022-12-17 19:37:35 -0500

    Final

---
+++ public/javascripts/script.js
@@ -0,0 +1,13 @@
+/**
+  Student Number : 301239985
+  Student Name: Mihir Darji
+  File: script.js
+
+**/
+/** javascript function - show message*/
+(function(){
+    function start(){
+        console.log("The application started");
+    }
+    start();
+})();
---
+++ readme.md
@@ -0,0 +1,4 @@
+
+# Assignment 1 - Express Portfolio Site
+# COMP229 Sec03– Web Application Development
+
